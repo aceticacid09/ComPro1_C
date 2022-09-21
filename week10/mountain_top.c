8ee0912c0bb8fdf02a773945ad29f47c3fda0dae
aceticacid09 <105485706+aceticacid09@users.noreply.github.com>
2022-09-21 17:48:40 +0700

    lab week10

--- week10/mountain_top.c
+++ week10/mountain_top.c
@@ -4,8 +4,7 @@ void main(){
     scanf("%d",&n);
     int arr[n][n];
 
-    //nPeak
-    //input
+    //input array
     int nPeak=0;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
@@ -29,32 +28,27 @@ void main(){
                 }
                 //printf("\n");
             }
-            //peak
+            //count peak
             if(count==8){
                 nPeak++;
             }
         }
     }
 
-
-    //find center
+    //output amount of peak
     printf("%d\n", nPeak);
+
+    //output peak location
     for(int centerR=1; centerR<n-1; centerR++){
         for(int centerC=1; centerC<n-1; centerC++){
-            //printf("%d\n",arr[centerR][centerC]);
-
-    //find 3x3 around center
             int count=0;
             for(int r=centerR-1; r<=centerR+1; r++){
                 for(int c=centerC-1; c<=centerC+1; c++){
-                    //printf("%d ",arr[r][c]);
                     if(arr[centerR][centerC]>arr[r][c]){
                         count++;
                     }
                 }
-                //printf("\n");
             }
-            //peak
             if(count==8){
                 printf("%d %d %d\n",arr[centerR][centerC],centerR,centerC);
             }
