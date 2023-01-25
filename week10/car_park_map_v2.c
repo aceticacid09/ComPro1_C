e59d37b12796c312276f8d526b5177c2e23e0cc6
aceticacid09 <105485706+aceticacid09@users.noreply.github.com>
2023-01-25 16:41:50 +0700

    Reconnecting
    -switching from windows11 to ubuntu

--- week10/car_park_map_v2.c
+++ week10/car_park_map_v2.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
 void main(){
-    int m,n,k;
-    int a,b;
+    int m,n,k,a,b;
     scanf("%d%d",&m,&n);
     char arr[m][n];
     scanf("%d",&k);
 
     for(int i=0; i<m; i++){
         for(int j=0; j<n; j++){
-            arr[i][j] = '_';
+            arr[i][j] = 0;
         }
     }
 
     for(int i=0; i<k; i++){
         scanf("%d%d",&a,&b);
-        arr[a-1][b-1] = 'x';
+        arr[a-1][b-1] = 1;
     }
 
     for(int i=0; i<m; i++){
         for(int j=0; j<n; j++){
-            printf("%c",arr[i][j]);
+            if(arr[a][b]==1){
+                printf("x");
+            } else {
+                printf("_");
+            }
         }
         printf("\n");
     }
 }
+
